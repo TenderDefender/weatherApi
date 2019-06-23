a6467b46c935ef78716fdcc7eeead4416bf930ea
Tender Defender <amaxasla322@gmail.com>
2019-06-23 13:27:12 +0300

    project setup

---
+++ js/gizmeteo.js
@@ -0,0 +1,79 @@
+// show Location
+let btn = document.querySelector('.loc');
+btn.addEventListener('click', initMap);
+btn.addEventListener('click', geoFindMe);
+let output = document.getElementById("out");
+let lat;
+let lng;
+function geoFindMe() {
+    if (!navigator.geolocation) {
+        output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+        return;
+    }
+
+    function success(position) {
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        lat = latitude;
+        lng = longitude;
+        console.log(longitude);
+        console.log(latitude);
+    }
+
+    function error() {
+        output.innerHTML = "Unable to retrieve your location";
+    }
+
+    output.innerHTML = "<p>Locating…</p>";
+    navigator.geolocation.getCurrentPosition(success, error);
+}
+
+function initMap() {
+    let options = {
+        center: {lat: lat, lng: lng},
+        zoom: 18,
+    };
+    let map = new google.maps.Map(document.getElementById("map"), options);
+}
+
+
+//weather
+
+const API_KEY = '37c550e61f6f3e0494726d44416a2ffc';
+btn.addEventListener('click', gettingWeather);
+btn.addEventListener('click', showWeather);
+let weatherData = {};
+
+function gettingWeather() {
+    const api_url = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`)
+        .then(function (response) {
+            return response.json();
+        }).then(data => weatherData = data);
+    console.log(weatherData);
+}
+
+function showWeather() {
+    if (weatherData.main.temp-(273.15)>=30) {
+        let showWeatherBox = document.querySelector('.show__weather');
+        let location=document.querySelector('.your__location');
+        let temperature=document.querySelector('.your__region__temperature');
+        let img = new Image();
+        img.src = "img/sun.jpg";
+        showWeatherBox.insertBefore(img,showWeather.lastChild);
+        location.innerHTML=(`Your location is ${weatherData.name}`);
+        temperature.innerHTML=(`outside temperature ${weatherData.main.temp-(273.15)}degrees Celsius`)
+    }
+    else if(weatherData.main.temp-(273.15)>=10&&weatherData.main.temp-(273.15)<30) {
+        let showWeatherBox = document.querySelector('.show__weather');
+        let location=document.querySelector('.your__location');
+        let temperature=document.querySelector('.your__region__temperature');
+        let img = new Image();
+        img.src = "img/cloudy.jpg";
+        showWeatherBox.insertBefore(img,showWeather.lastChild);
+        location.innerHTML=(`Your location is ${weatherData.name}`);
+        temperature.innerHTML=(`outside temperature ${weatherData.main.temp-(273.15)}degrees Celsius`)
+    }else{
+        console.log('Ah shit here we go again');
+    }
+}
+
---
+++ js/афаа.js
@@ -0,0 +1,12 @@
+
+function  lul() {
+    console.log('kek');
+}
+
+function asyncLul() {
+    setTimeout(()=>console.log('asyncKek'),0);
+}
+
+asyncLul();
+console.log('zalupa');
+lul();
